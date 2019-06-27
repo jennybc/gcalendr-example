22a32f683ee97dfcdf6b53cd081f0a63d94ad542
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-06-27 10:18:27 -0700

    Add comments

--- R/usage.R
+++ R/usage.R
@@ -2,6 +2,10 @@ library(gcalendr)
 
 options(
   gargle_oauth_cache = here::here(".secrets"),
+  # as long as .secrets/ holds exactly one token, this gives calendar_auth()
+  # to use it without requiring user to confirm
+  # if .secrets/ held more than one token, this could be set to an actual
+  # email address to disambiguate and prevent user interaction
   gargle_oauth_email = TRUE
 )
 
